d509e81dffd8247dd93b25185d869af327d76e8a
qiruihua <qiruihua@itcast.cn>
2019-09-06 19:59:31 +0800

    登陆功能

--- bbs/users/urls.py
+++ bbs/users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # 注册
     url(r'^register/$', views.RegisterView.as_view(), name='register'),
+    url(r'^login/$', views.LoginView.as_view(), name='login'),
 ]
--- bbs/users/views.py
+++ bbs/users/views.py
@@ -1,11 +1,7 @@
 import hashlib
-
 from django.shortcuts import render, redirect
-
-# Create your views here.
 from django.urls import reverse
 from django.views import View
-
 from users.models import User
 
 
@@ -60,3 +56,44 @@ class RegisterView(View):
         request.session['name'] = user.username
         # 5.返回相应
         return redirect(reverse('home:index'))
+
+class LoginView(View):
+
+    def get(self,request):
+
+        return render(request,'login.html')
+
+    def post(self,request):
+        """
+        1.接收参数
+        2.验证参数
+        3.判断用户信息
+        4.设置登陆信息
+        5.返回相应
+        """
+        # 1.接收参数
+        username = request.POST.get('username')
+        password = request.POST.get('password1')
+        # 2.验证参数
+        if not all([username,password]):
+            context = {
+                'username':username,
+                'password1':password,
+                'errmsg':'参数不全'
+            }
+            return render(request,'login.html',context=context)
+        # 3.判断用户信息
+        try:
+            user = User.objects.get(username=username,password=hashlib.md5(password.encode()).hexdigest())
+        except User.DoesNotExist:
+            context = {
+                'errmsg': '账号或密码错误'
+            }
+            return render(request,'login.html',context=context)
+
+        else:
+            # 4.设置登陆信息
+            request.session['id']=user.id
+            request.session['name']=user.username
+            # 5.返回相应
+            return redirect(reverse('home:index'))
