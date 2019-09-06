61550cd65e3359e65ce72cb94cdaaf082b87b981
qiruihua <qiruihua@itcast.cn>
2019-09-06 19:26:48 +0800

    注册实现

--- bbs/users/views.py
+++ bbs/users/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+import hashlib
+
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views import View
+
+from users.models import User
+
 
 class RegisterView(View):
     """用户注册"""
@@ -13,3 +18,44 @@ class RegisterView(View):
         :return: 注册界面
         """
         return render(request, 'register.html')
+
+    def post(self,request):
+        """
+        1.获取请求数据
+        2.验证请求数据
+        3.保存用户信息
+        4.设置登陆状态
+        5.返回相应
+        """
+        # 1.获取请求数据
+        username = request.POST.get('username')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        # 2.验证请求数据
+        if not all([username,password1,password2]):
+            context = {
+                'username':username,
+                'password1':password1,
+                'password2':password2,
+                'errmsg':'参数不全'
+            }
+            return render(request,'register.html',context=context)
+        # 3.保存用户信息
+        try:
+            user = User.objects.create(
+                username=username,
+                password=hashlib.md5(password1.encode()).hexdigest()
+            )
+        except Exception as e:
+            context = {
+                'username': username,
+                'password1': password1,
+                'password2': password2,
+                'errmsg': '创建失败'
+            }
+            return render(request,'register.html',context=context)
+        # 4.设置登陆状态
+        request.session['id'] = user.id
+        request.session['name'] = user.username
+        # 5.返回相应
+        return redirect('/')
