3168d5756b5ba12a6e89fb1e13a2c36c85346ce5
qiruihua <qiruihua@itcast.cn>
2019-09-09 19:01:11 +0800

    定义分类类,并执行迁移操作

--- bbs/home/models.py
+++ bbs/home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class CategoryModel(models.Model):
+
+    name=models.CharField(unique=True,max_length=20,verbose_name='分类名')
+    parent=models.ForeignKey('self',on_delete=models.SET_NULL,null=True,blank=True,verbose_name='父级分类id')
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table='tb_category'
+        verbose_name='分类管理'
+        verbose_name_plural=verbose_name
