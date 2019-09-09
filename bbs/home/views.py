e9b7a038447ae963f34de6d2c42f81fc0a26a51c
qiruihua <qiruihua@itcast.cn>
2019-09-09 19:56:34 +0800

    完善首页分类数据展示

--- bbs/home/views.py
+++ bbs/home/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
 from django.views import View
 
 # Create your views here.
-from home.models import CategoryModel
+from home.models import CategoryModel, ArticleModel
 
 
 class IndexView(View):
@@ -14,6 +15,14 @@ class IndexView(View):
         #查询分类数据
         categories = CategoryModel.objects.filter(parent__isnull=True)
 
+        for category in categories:
+            total_count=ArticleModel.objects.filter(category=category).count()
+            category.total_count=total_count
+
+            today=timezone.localdate()
+            today_count=ArticleModel.objects.filter(category=category,
+                                                    publish_time__gte=today).count()
+            category.today_count=today_count
         #组织上下文数据
         context = {
             'id':id,
