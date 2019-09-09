7756e7eb37c6e4195e8c76dff99168816454f26f
qiruihua <qiruihua@itcast.cn>
2019-09-09 19:23:47 +0800

    首页分类数据的获取和展示

--- bbs/home/views.py
+++ bbs/home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 
 # Create your views here.
+from home.models import CategoryModel
 
 
 class IndexView(View):
@@ -10,10 +11,14 @@ class IndexView(View):
         #获取session数据
         id = request.session.get('id')
         username=request.session.get('name')
+        #查询分类数据
+        categories = CategoryModel.objects.filter(parent__isnull=True)
+
         #组织上下文数据
         context = {
             'id':id,
-            'username':username
+            'username':username,
+            'categories': categories
         }
 
         return render(request,'index.html',context=context)
