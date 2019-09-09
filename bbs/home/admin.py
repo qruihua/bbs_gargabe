c0d0603d9d35ff8f54c8323af804fd91928f9028
qiruihua <qiruihua@itcast.cn>
2019-09-09 19:39:12 +0800

    admin站点添加对文章的管理

--- bbs/home/admin.py
+++ bbs/home/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from home.models import CategoryModel
+from home.models import CategoryModel,ArticleModel
 # Register your models here.
 
 admin.site.register(CategoryModel)
+admin.site.register(ArticleModel)
