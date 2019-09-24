14ce00055f0237c23114e7b5695e863a9dabff41
qiruihua <qiruihua@itcast.cn>
2019-09-24 18:33:31 +0800

    后台管理

--- bbs/home/admin.py
+++ bbs/home/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from home.models import CategoryModel,ArticleModel
+from home.models import CategoryModel,ArticleModel,CommentModel
 # Register your models here.
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ['id','name','parent_id']
+    list_per_page = 20
 
-admin.site.register(CategoryModel)
-admin.site.register(ArticleModel)
+class ArticleModelAdmin(admin.ModelAdmin):
+    list_display = ['id','title','content','category','user','status','read_count','comments','publish_time']
+    list_per_page = 20
+
+class CommentModelAdmin(admin.ModelAdmin):
+    list_display = ['id','content','article','user','parent','create_time']
+    list_per_page = 20
+
+admin.site.register(CategoryModel,CategoryModelAdmin)
+admin.site.register(ArticleModel,ArticleModelAdmin)
+admin.site.register(CommentModel,CommentModelAdmin)
+
